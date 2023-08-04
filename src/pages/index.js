1f5272e41062172c69e6d7fc1544ed60da09f1ac
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-04 21:41:37 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
         <Layout className='pt-0 md:p-16 sm:pt-8'>
           <div className='flex items-center justify-between w-full lg:flex-col'>
             <div className='w-1/2 md:w-full'>
-              <Image src={profilePic} alt="Anirudh Salaria" className='w-full h-auto lg:hidden md:inline-block md:w-full' priority
+              <Image src={profilePic} alt="Anirudh Salaria" className='w-[520px] h-[570px] lg:hidden md:inline-block sm:h-auto sm:w-full' priority
               sizes='(max-width: 768px) 100vw, 
               (max-width: 1200px) 50vw,
               50vw'

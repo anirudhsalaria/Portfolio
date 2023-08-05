8396aedc94bd5b00894234af82db67bf2017707f
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-05 15:35:53 +0530

    added darkmode in mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -72,7 +72,7 @@ const NavBar = () => {
 
 
       <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleCLick}>
-        
+
         <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
 
         <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0 ' : 'opacity-100'} `}></span>
@@ -135,17 +135,17 @@ const NavBar = () => {
 
 
           <motion.div
-            initial={{ scale: 0, opacity: 0, x:"-50%", y:"-50%" }}
+            initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
             animate={{ scale: 1, opacity: 1 }}
 
             className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
             bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
 
             <nav className="flex items-center flex-col justify-center">
-              <CustomMobileLink href='/' title="Home" 
-              className="" toggle={handleCLick} />
-              <CustomMobileLink href='/about' title="About" 
-              className="" toggle={handleCLick} />
+              <CustomMobileLink href='/' title="Home"
+                className="" toggle={handleCLick} />
+              <CustomMobileLink href='/about' title="About"
+                className="" toggle={handleCLick} />
               <CustomMobileLink href='/projects' title="Projects" className="" toggle={handleCLick} />
               <CustomMobileLink href='/articles' title="Articles" className="" toggle={handleCLick} />
             </nav>
@@ -175,13 +175,25 @@ const NavBar = () => {
                 <LinkedInIcon />
               </motion.a>
 
+              <button
+                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+     `}
+              >
+                {
+                  mode === "dark" ?
+                    <SunIcon className={"fill-dark"} />
+                    : <MoonIcon className={"fill-dark"} />
+                }
+              </button>
+
             </nav>
 
 
           </motion.div>
 
           : null
-          }
+      }
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
